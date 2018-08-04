b1af8474944f8b1d46cf9c091069a6fb452c3876
Jeremy Su <jeremysu0131@gmail.com>
2018-08-04 10:01:43 +0800

    Add babel and change some setting

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-// module.exports = require('./lib');
-
-const detect = require('./lib/index');
-
-const data = detect.readFile('./test/testfiles/test2.html');
-// console.log(detect.detectImage(data));
-console.log(detect.detectStrong(data, 15));
+module.exports = require('./lib');
--- src/index.js
+++ src/index.js
@@ -2,96 +2,94 @@ const cheerio = require('cheerio');
 
 const fs = require('fs');
 
-module.exports = {
-  /**
-   * @param {string} path - File location
-   *
-   * @returns {string} data - File Content
-   */
-  readFile: path => fs.readFileSync(path, 'utf8'),
+/**
+ * @param {string} path - File location
+ *
+ * @returns {string} data - File Content
+ */
+export const readFile = path => fs.readFileSync(path, 'utf8');
 
-  /**
-   * Detect <img> tag without alt attribute
-   * TODO:
-   *  - if the tag has alt attribute but value is null
-   * @param {string} data - Html text
-   * @returns {string} Check result
-   */
-  detectImage: (data) => {
-    const $ = cheerio.load(data);
-    let i = 0;
-    $('img').each((index, el) => {
-      if (!el.attribs.alt) i += 1;
-    });
-    return `There are ${i} <img> tag without alt attribute.`;
-  },
+/**
+ * Detect <img> tag without alt attribute
+ * TODO:
+ *  - if the tag has alt attribute but value is null
+ * @param {string} data - Html text
+ * @returns {string} Check result
+ */
+export const detectImage = (data) => {
+  const $ = cheerio.load(data);
+  let i = 0;
+  $('img').each((index, el) => {
+    if (!el.attribs.alt) i += 1;
+  });
+  return `There are ${i} <img> tag without alt attribute.`;
+};
 
-  /**
-   * Detect <a> link without rel attribute
-   * TODO:
-   *  - if the link has rel attribute but value is null
-   * @param {string} data - Html text
-   * @returns {string} Check result
-   */
-  detectLink: (data) => {
-    const $ = cheerio.load(data);
-    let i = 0;
-    $('a').each((index, el) => {
-      if (!el.attribs.rel) i += 1;
-    });
-    return `There are ${i} <a> tag without rel attribute.`;
-  },
+/**
+ * Detect <a> link without rel attribute
+ * TODO:
+ *  - if the link has rel attribute but value is null
+ * @param {string} data - Html text
+ * @returns {string} Check result
+ */
+export const detectLink = (data) => {
+  const $ = cheerio.load(data);
+  let i = 0;
+  $('a').each((index, el) => {
+    if (!el.attribs.rel) i += 1;
+  });
+  return `There are ${i} <a> tag without rel attribute.`;
+};
 
-  /**
-   * Detect <title> and <meta> tag
-   * @param {string} data - Html text
-   * @returns {string} Check result
-   */
-  detectHead: (data) => {
-    const $ = cheerio.load(data);
-    let hasTitle = false;
-    let hasDescription = false;
-    let hasKeywords = false;
-    if ($('head title').html()) hasTitle = true;
-    $('head meta').each((index, el) => {
-      if (el.attribs.name === 'description') hasDescription = true;
-      if (el.attribs.name === 'keywords') hasKeywords = true;
-    });
+/**
+ * Detect <title> and <meta> tag
+ * @param {string} data - Html text
+ * @returns {string} Check result
+ */
+export const detectHead = (data) => {
+  const $ = cheerio.load(data);
+  let hasTitle = false;
+  let hasDescription = false;
+  let hasKeywords = false;
+  if ($('head title').html()) hasTitle = true;
+  $('head meta').each((index, el) => {
+    if (el.attribs.name === 'description') hasDescription = true;
+    if (el.attribs.name === 'keywords') hasKeywords = true;
+  });
 
-    if (hasTitle && hasDescription && hasKeywords) {
-      return 'Header setup ok.';
-    }
+  if (hasTitle && hasDescription && hasKeywords) {
+    return 'Header setup ok.';
+  }
 
-    return `This HTML without${hasTitle ? '' : ' <title>'}${hasDescription ? '' : ' <meta name="descriptions" />'}${hasKeywords ? '' : ' <meta name="keywords" />'} tag.`;
-  },
+  return `This HTML without${hasTitle ? '' : ' <title>'}${hasDescription ? '' : ' <meta name="descriptions" />'}${hasKeywords ? '' : ' <meta name="keywords" />'} tag.`;
+};
 
-  /**
-   * Detect <strong> tag if exceed limit
-   * @param {string} data - Html text
-   * @param {number} [limit=15] - Limit of <strong>
-   * @returns {string} Check result
-   */
-  detectStrong: (data, limit = 15) => {
-    const $ = cheerio.load(data);
-    const i = $('strong').length;
+/**
+ * Detect <strong> tag if exceed limit
+ * @param {string} data - Html text
+ * @param {number} [limit=15] - Limit of <strong>
+ * @returns {string} Check result
+ */
+export const detectStrong = (data, limit = 15) => {
+  const $ = cheerio.load(data);
+  const i = $('strong').length;
 
-    if (i <= limit) {
-      return `<strong> tag isn't more than ${limit}. Total: ${i}.`;
-    }
-    return `<strong> tag is more than ${limit}. Total: ${i}.`;
-  },
+  if (i <= limit) {
+    return `<strong> tag isn't more than ${limit}. Total: ${i}.`;
+  }
+  return `<strong> tag is more than ${limit}. Total: ${i}.`;
+};
 
-  /**
-   * Detect <h1> tag if more than one
-   * @param {string} data - Html text
-   * @returns {string} Check result
-   */
-  detectH1: (data) => {
-    const $ = cheerio.load(data);
+/**
+ * Detect <h1> tag if more than one
+ * @param {string} data - Html text
+ * @returns {string} Check result
+ */
+export const detectH1 = (data) => {
+  const $ = cheerio.load(data);
 
-    if ($('h1').length <= 1) {
-      return '<h1> tag isn\'t more than one.';
-    }
-    return '<h1> tag is more than one.';
-  },
+  if ($('h1').length <= 1) {
+    return '<h1> tag isn\'t more than one.';
+  }
+  return '<h1> tag is more than one.';
 };
