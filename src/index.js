06f26bf8d7d73d310d41ab479ed47f1672069895
Jeremy Su <jeremysu@Jeremys-MacBook-Pro.local>
2018-08-19 18:39:46 +0800

    Change code for publish to npm

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-import fs from 'fs';
 import Check from './detect';
 import CheckStream from './detectStream';
 
-export {
-  Check,
-  CheckStream,
-};
-
-// Create a read stream
-const rs = fs.createReadStream('./test/testfiles/test1.html');
-
-// Pipe the stream to every method
-rs.pipe(CheckStream.detectImage())
-  .pipe(CheckStream.detectLink())
-  .pipe(CheckStream.detectTitle())
-  .pipe(CheckStream.detectMeta('keywords', 'descriptions'))
-  .pipe(CheckStream.detectStrong())
-  .pipe(CheckStream.detectH1())
-  .pipe(CheckStream.writeResultsToFile('./test.txt'));
+export { Check, CheckStream };
